11a060d1d133323d5896a6e0f03711e333404728
Anastasiya <musya@musya.note>
2024-01-21 20:34:48 +0400

    sanek loh

--- main.c
+++ main.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
 int main(int argc, char **argv) {
-	printf("Hello world");
+	int userAge = 5;
+	userAge = userAge + 4;
+	char *userName = "Dima";
+	float k = 1.885;
+	
+	printf("user age: %d, user name: %s, k: %f\n", userAge, userName, k);
 }
